7b9e0b9bcea991c19be41a5cfe5919a72e3ed0c5
José Amílcar González Miranda <joseamilcargonzalezmiranda@iMac-de-Juan.local>
2020-05-20 16:16:43 -0500

    Revert

--- app-resources/src/main/java/flyway/upt/V1_0_3__initial_db_content.java
+++ app-resources/src/main/java/flyway/upt/V1_0_3__initial_db_content.java
@@ -10,6 +10,6 @@ public class V1_0_3__initial_db_content implements JdbcMigration {
     public void migrate(Connection connection)
             throws Exception {
         // run setup based on json under /src/main/resources/setup/app-example.json
-        // DBHandler.setupAppContent(connection, "app-example");
+        DBHandler.setupAppContent(connection, "app-example");
     }
 }
