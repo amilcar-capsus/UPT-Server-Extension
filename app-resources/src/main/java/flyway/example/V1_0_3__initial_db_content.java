f19987de9fe3ecdc4aaec72c1e652218248cb6ba
amilcar-capsus <amilcar.gonzalez@capsus.mx>
2020-12-17 16:21:01 -0600

    Test

--- app-resources/src/main/java/flyway/example/V1_0_3__initial_db_content.java
+++ app-resources/src/main/java/flyway/example/V1_0_3__initial_db_content.java
@@ -3,14 +3,15 @@ package flyway.example;
 //import fi.nls.oskari.db.DBHandler;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.oskari.helpers.AppSetupHelper;
+import org.flywaydb.core.api.migration.Context;
 
 import java.sql.Connection;
 
 public class V1_0_3__initial_db_content extends BaseJavaMigration {
 
-    public void migrate(Connection connection)
+    public void migrate(Context context)
             throws Exception {
         // run setup based on json under /src/main/resources/setup/app-example.json
-        AppSetupHelper.create(connection, "app-example");
+        AppSetupHelper.create(context, "app-example");
     }
 }
