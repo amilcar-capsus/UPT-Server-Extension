2013b6d4b613c61589430246002ceecb721edbc3
amilcar-capsus <amilcar.gonzalez@capsus.mx>
2021-02-26 17:06:16 -0600

    Test

--- app-specific-code/src/main/java/org/oskari/example/GetWFSFeaturesHandlerTest.java
+++ app-specific-code/src/main/java/org/oskari/example/GetWFSFeaturesHandlerTest.java
@@ -2,6 +2,8 @@ package org.oskari.example;
 
 import static org.junit.Assert.assertTrue;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vividsolutions.jts.geom.Envelope;
 import fi.nls.oskari.control.feature.AbstractWFSFeaturesHandler;
 import fi.nls.oskari.control.feature.GetWFSFeaturesHandler;
@@ -9,6 +11,13 @@ import fi.nls.oskari.domain.map.OskariLayer;
 import fi.nls.oskari.map.layer.OskariLayerService;
 import fi.nls.oskari.util.PropertyUtil;
 import fi.nls.oskari.util.WFSDescribeFeatureHelper;
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
@@ -32,6 +41,8 @@ public class GetWFSFeaturesHandlerTest {
   private UPTGetWFSFeaturesHandler handler;
   private static String stProjection;
   private static OskariLayerService LAYER_SERVICE = ServiceFactory.getMapLayerService();
+  private JSONArray errors;
+  private ObjectMapper Obj;
 
   @Before
   public void init() {
@@ -45,7 +56,8 @@ public class GetWFSFeaturesHandlerTest {
 
   @Test
   @Ignore("Depends on an outside resource")
-  public void testGetFeatures(Long studyArea) throws Exception {
+  public void testGetFeatures(Long studyArea, String uuid) throws Exception {
+    String errorMsg = "Layers get";
     OskariLayer ml = LAYER_SERVICE.find(studyArea.intValue());
     CoordinateReferenceSystem webMercator = CRS.decode("EPSG:3857", true);
     // PropertyUtil.addProperty("oskari.native.srs", "EPSG:" + stProjection, true);
@@ -92,22 +104,127 @@ public class GetWFSFeaturesHandlerTest {
             AttributeDescriptor attr = attrIterator.next();
             names.put(attr.getLocalName());
           }
-        } finally {
-          System.out.println("ARRAY: " + names);
-        }
+        } finally {}
         //attributes.put(attributes);
         System.out.println("ID: " + feature.getID());
-        System.out.println("Names: " + names);
-        System.out.println("Attributes: " + attributes);
         for (int i = 0; i < names.length(); i++) {
-          System.out.println("Names: " + names.get(i));
-          System.out.println("Attributes: " + attributes.get(i));
           fullFeature.put(
             names.get(i).toString(),
             attributes.get(i).toString()
           );
         }
         System.out.println("Full Feature: " + fullFeature);
+        Iterator<String> featureKeys = fullFeature.keys();
+        String geomKey = "";
+        try {
+          while (featureKeys.hasNext()) {
+            String tmp = featureKeys.next();
+            if (tmp.contains("geom")) {
+              geomKey = tmp;
+            }
+          }
+        } finally {
+          System.out.println("NECESSARY KEY!!!!!!!!!! " + geomKey);
+        }
+        /* PostStatus status = new PostStatus();
+        String query = "";
+        try (
+          Connection connection = DriverManager.getConnection(
+            stURL,
+            stUser,
+            stPassword
+          );
+          PreparedStatement statement = connection.prepareStatement(
+            "INSERT INTO public.public_layer_data(public_layer_id, uuid, feature_id,property_json, geometry)VALUES ( ?, ?, ?,?,?);"
+          );
+        ) {
+          params.requireLoggedInUser();
+          ArrayList<String> roles = new UPTRoles()
+          .handleGet(params, params.getUser());
+          if (!roles.contains("uptadmin") && !roles.contains("uptuser")) {
+            throw new Exception("User privilege is not enough for this action");
+          }
+
+          statement.setLong(1, studyArea);
+          statement.setString(2, uuid);
+          statement.setString(3, feature.getID());
+          statement.setString(4, fullFeature);
+          statement.setString(5, fullFeature.get(Str.matches("(.*)geom(.*)")));
+
+          errors.put(
+            JSONHelper.createJSONObject(
+              Obj.writeValueAsString(
+                new PostStatus("OK", "Executing query: " + statement.toString())
+              )
+            )
+          );
+
+          status.message = statement.toString();
+          statement.execute();
+
+          errors.put(
+            JSONHelper.createJSONObject(
+              Obj.writeValueAsString(new PostStatus("OK", "Layer registered"))
+            )
+          );
+          ResponseHelper.writeResponse(
+            params,
+            new JSONObject().put("Errors", errors)
+          );
+        } catch (SQLException e) {
+          log.error(e);
+          try {
+            errors.put(
+              JSONHelper.createJSONObject(
+                Obj.writeValueAsString(new PostStatus("Error", e.toString()))
+              )
+            );
+            ResponseHelper.writeError(
+              params,
+              "",
+              500,
+              new JSONObject().put("Errors", errors)
+            );
+          } catch (JsonProcessingException ex) {
+            java
+              .util.logging.Logger.getLogger(STLayersHandler.class.getName())
+              .log(Level.SEVERE, null, ex);
+          } catch (JSONException ex) {
+            java
+              .util.logging.Logger.getLogger(STLayersHandler.class.getName())
+              .log(Level.SEVERE, null, ex);
+          }
+        } catch (JsonProcessingException ex) {
+          java
+            .util.logging.Logger.getLogger(STLayersHandler.class.getName())
+            .log(Level.SEVERE, null, ex);
+        } catch (JSONException ex) {
+          java
+            .util.logging.Logger.getLogger(STLayersHandler.class.getName())
+            .log(Level.SEVERE, null, ex);
+        } catch (Exception e) {
+          try {
+            errors.put(
+              JSONHelper.createJSONObject(
+                Obj.writeValueAsString(new PostStatus("Error", e.toString()))
+              )
+            );
+            ResponseHelper.writeError(
+              params,
+              "",
+              500,
+              new JSONObject().put("Errors", errors)
+            );
+          } catch (JsonProcessingException ex) {
+            java
+              .util.logging.Logger.getLogger(STLayersHandler.class.getName())
+              .log(Level.SEVERE, null, ex);
+          } catch (JSONException ex) {
+            java
+              .util.logging.Logger.getLogger(STLayersHandler.class.getName())
+              .log(Level.SEVERE, null, ex);
+          }
+        } */
       }
     } finally {
       iterator.close();
