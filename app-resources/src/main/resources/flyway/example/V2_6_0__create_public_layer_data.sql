0b3bb60007ae11672a7bcdbef8ebc5ee6ffae706
amilcar-capsus <amilcar.gonzalez@capsus.mx>
2021-03-01 16:49:16 -0600

    Test

--- app-resources/src/main/resources/flyway/example/V2_6_0__create_public_layer_data.sql
+++ app-resources/src/main/resources/flyway/example/V2_6_0__create_public_layer_data.sql
@@ -1,33 +1,3 @@
--- Table: public.public_layer_data
-
--- DROP TABLE public.public_layer_data;
-
-CREATE TABLE public.public_layer_data
-(
-    id bigint NOT NULL DEFAULT nextval('public_layer_data_id_seq'::regclass),
-    public_layer_id bigint NOT NULL,
-    uuid character varying(64) COLLATE pg_catalog."default",
-    feature_id character varying(64) COLLATE pg_catalog."default",
-    property_json json,
-    geometry geometry NOT NULL,
-    created timestamp with time zone NOT NULL DEFAULT now(),
-    updated timestamp with time zone,
-    CONSTRAINT "public_layer_data_pKey" PRIMARY KEY (id),
-    CONSTRAINT public_layer_data_user_layer_fkey FOREIGN KEY (public_layer_id)
-        REFERENCES public.oskari_maplayer (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
-
-ALTER TABLE public.public_layer_data
-    OWNER to oskari;
-COMMENT ON TABLE public.public_layer_data
-    IS 'User imported layer features';
-
 -- Index: public_layer_data_geom_idx
 
 -- DROP INDEX public.public_layer_data_geom_idx;
@@ -69,3 +39,33 @@ CREATE SEQUENCE public.public_layer_data_id_seq
 
 ALTER SEQUENCE public.public_layer_data_id_seq
     OWNER TO oskari;
+
+-- Table: public.public_layer_data
+
+-- DROP TABLE public.public_layer_data;
+
+CREATE TABLE public.public_layer_data
+(
+    id bigint NOT NULL DEFAULT nextval('public_layer_data_id_seq'::regclass),
+    public_layer_id bigint NOT NULL,
+    uuid character varying(64) COLLATE pg_catalog."default",
+    feature_id character varying(64) COLLATE pg_catalog."default",
+    property_json json,
+    geometry geometry NOT NULL,
+    created timestamp with time zone NOT NULL DEFAULT now(),
+    updated timestamp with time zone,
+    CONSTRAINT "public_layer_data_pKey" PRIMARY KEY (id),
+    CONSTRAINT public_layer_data_user_layer_fkey FOREIGN KEY (public_layer_id)
+        REFERENCES public.oskari_maplayer (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.public_layer_data
+    OWNER to oskari;
+COMMENT ON TABLE public.public_layer_data
+    IS 'User imported layer features';
