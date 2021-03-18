6cc967244f861c0d8493d8178d912f9894c886db
amilcar-capsus <amilcar.gonzalez@capsus.mx>
2021-03-18 15:50:29 -0600

    Test

--- app-resources/src/main/resources/flyway/example/V2_2_1__create_stored_st_procedure.sql
+++ app-resources/src/main/resources/flyway/example/V2_2_1__create_stored_st_procedure.sql
@@ -127,6 +127,13 @@ BEGIN
             INNER JOIN st_filters ON user_layer_data.user_layer_id = st_filters.user_layer_id
         WHERE
             st_filters.id = ANY (filters_list))
+        UNION SELECT
+            geometry
+        FROM
+            public_layer_data
+            INNER JOIN st_public_filters ON public_layer_data.public_layer_id = st_public_filters.public_layer_id
+        WHERE
+            st_public_filters.id = ANY (public_filters_list)
     LOOP
         IF first_time THEN
             intersected = filter_pol;
@@ -369,6 +376,13 @@ BEGIN
             INNER JOIN st_public_filters ON public_layer_data.public_layer_id = st_public_filters.public_layer_id
         WHERE
             st_public_filters.id = ANY (public_filters_list))
+        UNION SELECT
+            geometry
+        FROM
+            user_layer_data
+            INNER JOIN st_filters ON user_layer_data.user_layer_id = st_filters.user_layer_id
+        WHERE
+            st_filters.id = ANY (filters_list))
     LOOP
         IF public_first_time THEN
             public_intersected = public_filter_pol;
