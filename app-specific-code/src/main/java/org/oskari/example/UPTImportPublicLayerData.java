f333dbd6aa37795933665f1b81ab7cf8f014083b
amilcar-capsus <amilcar.gonzalez@capsus.mx>
2021-03-28 10:34:51 -0600

    Test

--- app-specific-code/src/main/java/org/oskari/example/UPTImportPublicLayerData.java
+++ app-specific-code/src/main/java/org/oskari/example/UPTImportPublicLayerData.java
@@ -141,135 +141,6 @@ public class UPTImportPublicLayerData extends RestActionHandler {
           .log(Level.SEVERE, null, ex);
       }
     }
-    /* try (
-      Connection connection = DriverManager.getConnection(
-        stURL,
-        stUser,
-        stPassword
-      );
-      PreparedStatement statement = connection.prepareStatement(
-        "with study_area as(\n" +
-        "    select geometry from user_layer_data where user_layer_id=?\n" +
-        "), user_layers as(\n" +
-        "    select distinct st_layers.id as id, st_layers.st_layer_label, st_layer_label as label ,st_layers.user_layer_id,layer_field,layer_mmu_code,is_public\n" +
-        "    from st_layers\n" +
-        "    inner join user_layer_data on user_layer_data.user_layer_id = st_layers.user_layer_id\n" +
-        "    inner join user_layer on user_layer.id = user_layer_data.user_layer_id\n" +
-        "    left join upt_user_layer_scope on upt_user_layer_scope.user_layer_id=user_layer.id\n" +
-        "    , study_area\n" +
-        "    where \n" +
-        "    (user_layer.uuid=? or upt_user_layer_scope.is_public=1) and\n" +
-        "    st_intersects(study_area.geometry,user_layer_data.geometry)\n" +
-        ")\n" +
-        "select id, st_layer_label, label,user_layer_id,layer_field,layer_mmu_code,is_public from user_layers"
-      );
-    ) {
-      params.requireLoggedInUser();
-      ArrayList<String> roles = new UPTRoles()
-      .handleGet(params, params.getUser());
-      if (!roles.contains("uptadmin") && !roles.contains("uptuser")) {
-        throw new Exception("User privilege is not enough for this action");
-      }
-
-      statement.setLong(1, study_area);
-      statement.setString(2, user_uuid);
-
-      errors.put(
-        JSONHelper.createJSONObject(
-          Obj.writeValueAsString(
-            new PostStatus("OK", "Executing query: " + statement.toString())
-          )
-        )
-      );
-      //statement.setLong(2, user_id);
-      boolean status = statement.execute();
-      if (status) {
-        ResultSet data = statement.getResultSet();
-        while (data.next()) {
-          STLayers layer = new STLayers();
-          layer.id = data.getLong("id");
-          layer.label = data.getString("label");
-          layer.st_layer_label = data.getString("st_layer_label");
-          layer.user_layer_id = data.getLong("user_layer_id");
-          layer.layer_field = data.getString("layer_field");
-          layer.layer_mmu_code = data.getString("layer_mmu_code");
-          modules.add(layer);
-        }
-      } else {
-        STLayers layer = new STLayers();
-        layer.id = -1L;
-        layer.label = statement.toString();
-        modules.add(layer);
-      }
-
-      JSONArray out = new JSONArray();
-      for (STLayers index : modules) {
-        //Convert to Json Object
-        ObjectMapper Obj = new ObjectMapper();
-        final JSONObject json = JSONHelper.createJSONObject(
-          Obj.writeValueAsString(index)
-        );
-        out.put(json);
-      }
-
-      errors.put(
-        JSONHelper.createJSONObject(
-          Obj.writeValueAsString(new PostStatus("OK", "Layers executed"))
-        )
-      );
-
-      ResponseHelper.writeResponse(params, out);
-    } catch (SQLException e) {
-      try {
-        errors.put(
-          JSONHelper.createJSONObject(
-            Obj.writeValueAsString(new PostStatus("Error", e.toString()))
-          )
-        );
-        ResponseHelper.writeError(
-          params,
-          "",
-          500,
-          new JSONObject().put("Errors", errors)
-        );
-      } catch (JsonProcessingException ex) {
-        java
-          .util.logging.Logger.getLogger(STLayersHandler.class.getName())
-          .log(Level.SEVERE, null, ex);
-      } catch (JSONException ex) {
-        java
-          .util.logging.Logger.getLogger(STLayersHandler.class.getName())
-          .log(Level.SEVERE, null, ex);
-      }
-      errorMsg = errorMsg + e.toString();
-      log.error(e, errorMsg);
-    } catch (JsonProcessingException ex) {
-      java
-        .util.logging.Logger.getLogger(STLayersHandler.class.getName())
-        .log(Level.SEVERE, null, ex);
-    } catch (Exception e) {
-      try {
-        errors.put(
-          JSONHelper.createJSONObject(
-            Obj.writeValueAsString(new PostStatus("Error", e.toString()))
-          )
-        );
-        ResponseHelper.writeError(
-          params,
-          "",
-          500,
-          new JSONObject().put("Errors", errors)
-        );
-      } catch (JsonProcessingException ex) {
-        java
-          .util.logging.Logger.getLogger(STLayersHandler.class.getName())
-          .log(Level.SEVERE, null, ex);
-      } catch (JSONException ex) {
-        java
-          .util.logging.Logger.getLogger(STLayersHandler.class.getName())
-          .log(Level.SEVERE, null, ex);
-      }
-    } */
   }
 
   @Override
@@ -279,6 +150,9 @@ public class UPTImportPublicLayerData extends RestActionHandler {
     String query = "";
     Long user_id = params.getUser().getId();
     user_uuid = params.getUser().getUuid();
+
+    System.out.println("USER ID: " + user_id);
+    System.out.println("USER UUID: " + user_uuid);
     Long study_area;
     study_area = Long.parseLong(params.getRequiredParam("study_area"));
     try (
