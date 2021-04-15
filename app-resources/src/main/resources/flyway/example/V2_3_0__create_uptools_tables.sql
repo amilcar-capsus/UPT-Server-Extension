7494fb65a99baffb4799d7eb70a41f5f041dbab6
amilcar-capsus <amilcar.gonzalez@capsus.mx>
2021-04-15 14:06:18 -0500

    Remove record on normalization

--- app-resources/src/main/resources/flyway/example/V2_3_0__create_uptools_tables.sql
+++ app-resources/src/main/resources/flyway/example/V2_3_0__create_uptools_tables.sql
@@ -2184,6 +2184,7 @@ begin;
 	VALUES (1,'english','Observe'),
         (2,'english','Benchmark'),
         (3,'english','Standardize') on conflict(value,  language) do nothing;
+    DELETE FROM public.st_normalization_method_options WHERE id = 1;
 
     create table if not exists st_normalization_type_options(
             id serial not null,
