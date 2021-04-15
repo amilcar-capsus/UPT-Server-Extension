ca9ea553bc39b693f219c33f225530cab6df8b0b
amilcar-capsus <amilcar.gonzalez@capsus.mx>
2021-04-15 14:04:30 -0500

    Change reference to benchmark

--- app-resources/src/main/resources/flyway/example/V1_3_0__create_uptools_tables.sql
+++ app-resources/src/main/resources/flyway/example/V1_3_0__create_uptools_tables.sql
@@ -2088,7 +2088,7 @@ begin;
         CONSTRAINT st_normalization_name_key UNIQUE (name)
     );
 
-    insert into st_normalization(name) values ('observe'),('reference'),('standarize') on conflict(name) do nothing;
+    insert into st_normalization(name) values ('observe'),('benchmark'),('standardize') on conflict(name) do nothing;
 
     create table if not exists st_normalization_method_options(
 	id serial not null,
@@ -2108,7 +2108,7 @@ begin;
     INSERT INTO public.st_normalization_method_options(
 	value, language, label)
 	VALUES (1,'english','Observe'),
-        (2,'english','Reference'),
+        (2,'english','Benchmark'),
         (3,'english','Standardize') on conflict(value,  language) do nothing;
     DELETE FROM public.st_normalization_method_options WHERE id = 1;
 
