3831fe2bb5f068f7e3674671736e67674c719723
amilcar-capsus <amilcar.gonzalez@capsus.mx>
2021-04-19 16:36:02 -0500

    Test fix to scripts

--- app-resources/src/main/resources/flyway/example/run-upt-scripts.sql
+++ app-resources/src/main/resources/flyway/example/run-upt-scripts.sql
@@ -1,5 +1,5 @@
-\i V1_2_0__create_stored_st_procedure.sql
-\i V1_3_0__create_uptools_tables.sql
-\i V1_4_0__upt_initial-users.sql
-\i V1_5_0__create_uptools_tables.sql
-\i V1_6_0__create_uptools_tables.sql
+\i ./V1_2_0__create_stored_st_procedure.sql
+\i ./V1_3_0__create_uptools_tables.sql
+\i ./V1_4_0__upt_initial-users.sql
+\i ./V1_5_0__create_uptools_tables.sql
+\i ./V1_6_0__create_uptools_tables.sql
