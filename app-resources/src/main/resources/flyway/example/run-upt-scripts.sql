b0b777a72721cae0f058d25da17fe293bff9d3f6
amilcar-capsus <amilcar.gonzalez@capsus.mx>
2021-04-19 16:43:33 -0500

    Fix scripts

--- app-resources/src/main/resources/flyway/example/run-upt-scripts.sql
+++ app-resources/src/main/resources/flyway/example/run-upt-scripts.sql
@@ -1,6 +1,6 @@
-\i V2_2_1__create_stored_st_procedure.sql
-\i V2_2_2__create_stored_st_procedure_public.sql
-\i V2_3_0__create_uptools_tables.sql
-\i V2_4_0__create_uptools_tables.sql
-\i V2_5_0__create_uptools_tables.sql
-\i V2_6_0__create_public_layer_data.sql
+\ir V2_2_1__create_stored_st_procedure.sql
+\ir V2_2_2__create_stored_st_procedure_public.sql
+\ir V2_3_0__create_uptools_tables.sql
+\ir V2_4_0__create_uptools_tables.sql
+\ir V2_5_0__create_uptools_tables.sql
+\ir V2_6_0__create_public_layer_data.sql
